ade3dbe041de0608d9316df6354bec30252c68de
GaboGomez09 <gomezgabriel09@gmail.com>
2020-12-30 23:51:26 -0600

    added store model

--- app.py
+++ app.py
@@ -11,7 +11,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "gabo"
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 jwt = JWT(app, authenticate, identity) #auth
+
 
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
--- resources/item.py
+++ resources/item.py
@@ -8,6 +8,10 @@ class Item(Resource):
         type=float,
         required=True,
         help="This field cannot be left blank")
+    parser.add_argument("store_id",
+        type=int,
+        required=True,
+        help="Every item needs a store_id")
 
     @jwt_required()
     def get(self, name):
@@ -22,7 +26,7 @@ class Item(Resource):
         
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data["price"])
+        item = ItemModel(name, **data)
         try:
             item.save_to_db()
         except:
@@ -44,7 +48,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data["price"])
+            item = ItemModel(name, **data)
         else:
             item.price = data["price"]
 
